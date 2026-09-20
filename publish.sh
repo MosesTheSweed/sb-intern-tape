13a575177e7545d8577d889cd1fa1a7cdb78a8f6
Holmes <holmes@pop-os.lan>
2026-09-20 11:25:53 -0400

    add publish.sh

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+cd /home/holmes/sb-intern-tape
+python3 crawl_interns.py --out interns.json --workers 8
+git add interns.json
+git diff --cached --quiet && exit 0
+git commit -m "intern snapshot $(date -u +%Y-%m-%dT%H:%MZ)"
+git push
